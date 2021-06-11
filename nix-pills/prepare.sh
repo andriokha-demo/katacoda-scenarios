e503dbad748b8c504f8b6b2b036d1ef49584c6d4
Andy Fowlston <andy@fabbdev.com>
2021-06-11 20:45:48 +0100

    Revert changes to setup
    Echoing to null didn't seem to work.

--- nix-pills/prepare.sh
+++ nix-pills/prepare.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 user=nix
-(
-  useradd -m $user;
-  mkdir -m 0755 /nix && chown $user /nix;
 
-  sudo -s -u $user;
-  cd "$HOME" || exit 1;
-) > /dev/null
+useradd -m $user
+mkdir -m 0755 /nix && chown $user /nix
+
+sudo -s -u $user
+cd "$HOME" || exit 1
